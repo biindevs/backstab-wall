89ba68e2b51848fa9892caf40e39142e081f51a3
Bin-baz Akilan <binbaz.akilan@outlook.com>
2023-06-21 02:20:45 +0800

    add responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ const Homepage = () => {
         </button>
         {isModalOpen && <MessageForm closeModal={closeModal} />}
       </div>
-      <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 justify-items-center items-center gap-6 lg:gap-12 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
+      <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 justify-items-center items-center gap-10 p-10">
         {messages &&
           messages.map((message) => (
             <MessageBox
